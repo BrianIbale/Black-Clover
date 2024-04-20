eaf74b67b32b4b3889076e5b772ee2af889e7e53
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-21 06:58:35 +0800

    Create models and activate models

--- hg_project/galleries/models.py
+++ hg_project/galleries/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class People(models.Model):
+    people_text = models.CharField(max_length=200)
+
+class Person(models.Model):
+    people = models.ForeignKey(People, on_delete=models.CASCADE)
+    person_text = models.CharField(max_length=200)
+    person_subtext = models.TextField()
+
+class Events(models.Model):
+    events_text = models.CharField(max_length=200)
+
+class Event(models.Model):
+    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    event_text = models.CharField(max_length=200)
+    event_subtext = models.TextField()
