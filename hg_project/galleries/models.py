f0d02e2e4b70cc94bd5d52461d50275ed8b4dd2c
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-21 21:00:48 +0800

    Update database and create admin page

--- hg_project/galleries/models.py
+++ hg_project/galleries/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class People(models.Model):
-    people_text = models.CharField(max_length=200)
+class Person(models.Model):
+    person_text = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.people_text
+        return self.person_text
 
-class Person(models.Model):
-    people = models.ForeignKey(People, on_delete=models.CASCADE)
+class PersonDetail(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
     person_text = models.CharField(max_length=200)
     person_subtext = models.TextField()
 
     def __str__(self):
-        return self.person_subtext
-
-class Events(models.Model):
-    events_text = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.events_text
+        return f"{self.person_subtext[:50]}..."
 
 class Event(models.Model):
-    event = models.ForeignKey(Events, on_delete=models.CASCADE)
+    event_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.event_text
+
+class EventDetail(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
     event_text = models.CharField(max_length=200)
     event_subtext = models.TextField()
 
