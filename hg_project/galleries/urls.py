9cbcccaa32d1524e554431eaa58ea43265c265a8
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-22 07:58:35 +0800

    Create urls, views, and templates for home page, person, and person details

--- hg_project/galleries/urls.py
+++ hg_project/galleries/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path("", views.index, name="index"),
 
     # Page that shows all persons.
-    path('persons', views.persons, name='persons'),
+    path('persons/', views.persons, name='persons'),
     # Detail page for a single person.
-    path('persons/<int:person_id>/', views.person, name='person'),
+    path('persons/<int:person_id>/', views.person_detail, name='person_detail'),
     # Page for adding a new person.
     path('new_person/', views.new_person, name='new_person'),
     # Page for editing a person.
--- hg_project/galleries/views.py
+++ hg_project/galleries/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Person, PersonDetail, Event, EventDetail
+
 # Create your views here.
 def index(request):
     """The home page for History Gallery."""
-    return HttpResponse("Hello, world. You're at the galleries index.")
+    return render(request, 'galleries/index.html')
 
 
 def persons(request):
     """Show all persons."""
+    persons = Person.objects.all()
+    context = {'persons': persons}
+    return render(request, 'galleries/persons.html', context)
 
 
-def person(request):
+def person_detail(request, person_id):
     """Show a single person's details."""
+    person = Person.objects.get(id=person_id)
+
+    person_details = person.persondetail_set.all()
+    context = {'person': person, 'person_details': person_details}
+    return render(request, 'galleries/person_detail.html', context)
 
 
 def new_person(request):
