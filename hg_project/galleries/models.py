ffcbab7cdab6ad1421ea1abdfa02af0baffb5895
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-21 08:26:53 +0800

    Update galleries models and use Django API to create data for people and person models

--- hg_project/galleries/models.py
+++ hg_project/galleries/models.py
@@ -4,15 +4,27 @@ from django.db import models
 class People(models.Model):
     people_text = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.people_text
+
 class Person(models.Model):
     people = models.ForeignKey(People, on_delete=models.CASCADE)
     person_text = models.CharField(max_length=200)
     person_subtext = models.TextField()
 
+    def __str__(self):
+        return self.person_subtext
+
 class Events(models.Model):
     events_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.events_text
 
 class Event(models.Model):
     event = models.ForeignKey(Events, on_delete=models.CASCADE)
     event_text = models.CharField(max_length=200)
     event_subtext = models.TextField()
+
+    def __str__(self):
+        return self.event_subtext
