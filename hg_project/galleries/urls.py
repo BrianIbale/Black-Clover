bd489cd83822a918077244da9a194c0da5212d38
BrianIbale <ibalebrianmathew@gmail.com>
2024-04-22 06:24:06 +0800

    Create initial urls and views for galleries app

--- hg_project/galleries/urls.py
+++ hg_project/galleries/urls.py
@@ -1,7 +1,29 @@
+"""Defines URL patterns for galleries."""
+
 from django.urls import path
 
 from . import views
 
+app_name = 'galleries'
 urlpatterns = [
+    # Home page
     path("", views.index, name="index"),
+
+    # Page that shows all persons.
+    path('persons', views.persons, name='persons'),
+    # Detail page for a single person.
+    path('persons/<int:person_id>/', views.person, name='person'),
+    # Page for adding a new person.
+    path('new_person/', views.new_person, name='new_person'),
+    # Page for editing a person.
+    path('edit_person/<int:person_id>/', views.edit_person, name='edit_person'),
+    
+    # Page that shows all events.
+    path('events', views.events, name='events'),
+    # Detail page for a single event.
+    path('events/<int:event_id>/', views.event, name='event'),
+    # Page for adding a new event.
+    path('new_event/', views.new_event, name='new_event'),
+    # Page for editing a person.
+    path('edit_event/<int:event_id>/', views.edit_event, name='edit_event'),
 ]
--- hg_project/galleries/views.py
+++ hg_project/galleries/views.py
@@ -3,4 +3,37 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    """The home page for History Gallery."""
     return HttpResponse("Hello, world. You're at the galleries index.")
+
+
+def persons(request):
+    """Show all persons."""
+
+
+def person(request):
+    """Show a single person's details."""
+
+
+def new_person(request):
+    """Add a new person."""
+
+
+def edit_person(request):
+    """Edit an existing person."""
+
+
+def events(request):
+    """Show all events."""
+
+
+def event(request):
+    """Show a single event's details."""
+
+
+def new_event(request):
+    """Add a new event."""
+
+
+def edit_event(request):
+    """Edit an existing event."""
